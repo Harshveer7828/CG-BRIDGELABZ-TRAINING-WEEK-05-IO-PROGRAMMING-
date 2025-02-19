1bf35e61d462e45dd267f6581d9301a7a555bdc2
harshveer7828 <harshveer7828@gmail.com>
2025-02-19 10:24:06 +0530

    [Harsh veer singh] Add. Search in a csv file code file added✨

---
+++ Day01/src/main/java/com/capgemini/intermidiate_problems/search_in_csv_file/Employee.java
@@ -0,0 +1,24 @@
+package com.capgemini.intermidiate_problems.search_in_csv_file;
+
+public class Employee {
+    private String name;
+    private String department;
+    private double salary;
+
+    public Employee(String name, String department, double salary) {
+        this.name = name;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + name + ", Department: " + department + ", Salary: " + salary;
+    }
+
+
+}
